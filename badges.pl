66d6b240854383631ee4e15641d37b261abda5e2
Alex Drummond <a.d.drummond@gmail.com>
2011-03-23 15:31:55 -0400

    Minor.

--- badges.pl
+++ badges.pl
@@ -1,3 +1,4 @@
+
 use warnings;
 use strict;
 
@@ -27,15 +28,23 @@ use constant {
 
     TITLE => "Mayfest 2011",
 
-    SPACING_BELOW_TITLE => 0.25,
-    NAME_RIGHT_NUDGE => 0.02,
+    NAME_CENTER_OFFSET => -0.1,
+    SPACING_BELOW_NAME => 0.2,
+    NAME_RIGHT_NUDGE => 0,
+    INSTITUTION_RIGHT_NUDGE => 0,
 
     TITLE_MAX_FONT_SIZE => 50,
-    NAME_MAX_FONT_SIZE => 25,
+    NAME_MAX_FONT_SIZE => 20,
+    INSTITUTION_MAX_FONT_SIZE => 15,
+
+    FONT_SIZE_EPSILON => 2,
 };
-use constant TITLE_COLOR => (0, 0, 0);
+use constant TITLE_COLOR => (182/255, 0, 0);
 use constant NAME_COLOR => (0, 0, 0);
-use constant FONT => ("Museo Sans", "normal", "normal");
+use constant INSTITUTION_COLOR => (0, 0, 0);
+use constant TITLE_FONT => ("Verdana", "normal", "normal");
+use constant NAME_FONT => ("Verdana", "normal", "normal");
+use constant INSTITUTION_FONT => ("Verdana", "italic", "normal");
 
 sub in_to_pt { return shift() * PTS_IN_ONE_IN }
 sub in_to_pti { return int(shift() * PTS_IN_ONE_IN); }
@@ -114,14 +123,14 @@ sub draw_badge {
         } $cr, $logo_surface_pattern;
 
         # Draw title.
-        $cr->select_font_face(FONT);
+        $cr->select_font_face(TITLE_FONT);
         $cr->set_font_size(
             get_best_font_size(
                 pt_to_in(TITLE_MAX_FONT_SIZE),
                 TITLE,
                 pt_to_in($badge_width_in_pts-$logo_portion_pts) - BADGE_RIGHT_MARGIN,
                 BADGE_HEIGHT, # Doesn't matter what we give here since width is the real constraint.
-                pt_to_in(2)
+                pt_to_in(FONT_SIZE_EPSILON)
             )
         );
         my $logo_vportion = pt_to_in($logo_surface->get_height() / $scale_factor);
@@ -132,21 +141,40 @@ sub draw_badge {
         $cr->fill();
 
         # Draw person's name.
-        $cr->select_font_face(FONT);
+        $cr->select_font_face(NAME_FONT);
         $cr->set_font_size(
             get_best_font_size(
                 pt_to_in(NAME_MAX_FONT_SIZE),
                 $info->{name},
                 BADGE_WIDTH - BADGE_LEFT_MARGIN - BADGE_RIGHT_MARGIN,
                 BADGE_HEIGHT, # Doesn't matter what we give here since width is the real constraint.
-                pt_to_in(2)
+                pt_to_in(FONT_SIZE_EPSILON)
             )
         );
         my $name_extents = $cr->text_extents($info->{name});
-        $cr->move_to(NAME_RIGHT_NUDGE + ((BADGE_WIDTH - BADGE_LEFT_MARGIN - BADGE_RIGHT_MARGIN - $name_extents->{width}) / 2),
-                     $logo_vportion + SPACING_BELOW_TITLE - $name_extents->{y_bearing});
+        my $name_y;
+        $cr->move_to(BADGE_LEFT_MARGIN + NAME_RIGHT_NUDGE + ((BADGE_WIDTH - BADGE_LEFT_MARGIN - BADGE_RIGHT_MARGIN - $name_extents->{width}) / 2),
+                     $name_y = NAME_CENTER_OFFSET + (BADGE_HEIGHT / 2) - $name_extents->{y_bearing});
         $cr->text_path($info->{name});
         $cr->set_source_rgb(NAME_COLOR);
+        $cr->fill();
+
+        # Draw instutition.
+        $cr->select_font_face(INSTITUTION_FONT);
+        $cr->set_font_size(
+            get_best_font_size(
+                pt_to_in(INSTITUTION_MAX_FONT_SIZE),
+                $info->{institution},
+                BADGE_WIDTH - BADGE_LEFT_MARGIN - BADGE_RIGHT_MARGIN,
+                BADGE_HEIGHT, # Doesn't matter what we give here since width is the real constraint.
+                pt_to_in(FONT_SIZE_EPSILON)
+            )
+        );
+        my $inst_extents = $cr->text_extents($info->{institution});
+        $cr->move_to(BADGE_LEFT_MARGIN + INSTITUTION_RIGHT_NUDGE + ((BADGE_WIDTH - BADGE_LEFT_MARGIN - BADGE_RIGHT_MARGIN - $inst_extents->{width}) / 2),
+                     $name_y + SPACING_BELOW_NAME - $inst_extents->{y_bearing});
+        $cr->text_path($info->{institution});
+        $cr->set_source_rgb(INSTITUTION_COLOR);
         $cr->fill();
     } $cr, Cairo::Matrix->init(1, 0, 0, 1, $x + BADGE_LEFT_MARGIN, $y + BADGE_TOP_MARGIN)
            ->multiply(Cairo::Matrix->init(in_to_pt(1), 0, 0, in_to_pt(1), 1, 1));
@@ -177,7 +205,7 @@ draw_badges([
     { name => "Collin Phillips",
       institution => "University of Maryland"
     },
-    { name => "Sir Person Bearing a Very Long Name",
+    { name => "Sir Person Bearing a Very Long Name Indeed",
       institution => "Prolixity Institute"
     }
 ])
