1408d20c162a035d2c49b94ff339072a2fd38087
Alex Drummond <a.d.drummond@gmail.com>
2011-04-01 01:25:43 -0400

    More work on schedule.

--- mayfest.py
+++ mayfest.py
@@ -11,6 +11,7 @@ import fcntl
 import time
 import datetime
 import re
+import itertools
 
 def lock_and_open(filename, mode):
     if os.path.exists(filename):
@@ -97,8 +98,8 @@ with open(os.path.join(conf.WORKING_DIR, "schedule.txt")) as schedule_f:
             event = { }
             try:
                 iso8601 = "%Y-%m-%dT%H:%M"
-                event['start_datetime'] = time.strptime(fields[0], iso8601)
-                event['end_datetime'] = time.strptime(datetime.datetime(*event['start_datetime'][:5]).strftime("%Y-%m-%dT" + fields[1]), iso8601)
+                event['start_datetime'] = datetime.datetime(*time.strptime(fields[0], iso8601)[:5])
+                event['end_datetime'] = datetime.datetime(*time.strptime(event['start_datetime'].strftime("%Y-%m-%dT" + fields[1]), iso8601)[:5])
             except ValueError, e:
                 raise Exception("Error parsing date/time in schedule.txt: use ISO 8601 format (see e.g. Wikipedia article) (%s)." % str(e))
             info = fields[2]
@@ -110,10 +111,21 @@ with open(os.path.join(conf.WORKING_DIR, "schedule.txt")) as schedule_f:
                 if len(s) < 1:
                     raise Exception("Unknown speaker referenced in schedule.txt: '%s'" % m.group(1))
                 event['abstractfile'] = s[0]['abstractfile']
+                event['homepage'] = s[0]['homepage']
+            else:
+                event['info'] = info
             event_list.append(event)
+event_list.sort(key=lambda event: event['start_datetime'])
+# Group events by day (makes table rendering logic simpler in template).
+event_list_by_days = list(itertools.imap(lambda x: list(x[1]),
+    itertools.groupby(event_list, lambda e: (e['start_datetime'].year,
+                                             e['start_datetime'].month,
+                                             e['start_datetime'].day))
+))
+print event_list_by_days
 class schedule:
     def GET(self):
-        return render_wrapper('Schedule', render.schedule())
+        return render_wrapper('Schedule', render.schedule(event_list_by_days))
 
 class register:
     JS_EXTRAS = [conf.url_for('/static/register.js'), conf.url_for('/static/jquery.simplemodal.js')]
